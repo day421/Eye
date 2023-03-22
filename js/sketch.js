0ca091fa923f06039f479204e6a152f8b8c1d95e
day421 <tree421@pm.me>
2023-03-22 23:34:08 +0900

    Eye
    과제

---
+++ js/sketch.js
@@ -0,0 +1,48 @@
+var e1, e2, e3;
+
+function setup() {
+  createCanvas(640, 360);
+  noStroke();
+  e1 = new Eye(250, 20, 120);
+  e2 = new Eye(160, 175, 85);
+  e3 = new Eye(410, 220, 220);
+}
+
+function draw() {
+  background(102);
+
+  e1.update(mouseX, mouseY);
+  e2.update(mouseX, mouseY);
+  e3.update(mouseX, mouseY);
+
+  e1.display();
+  e2.display();
+  e3.display();
+}
+
+function Eye(tx, ty, ts) {
+  this.x = tx;
+  this.y = ty;
+
+  this.size = ts;
+  this.angle = 0.0;
+
+  this.update = function (mx, my) {
+    this.angle = atan2(my - this.y, mx - this.x);
+  };
+
+  this.display = function () {
+    push();
+    translate(this.x, this.y);
+
+    fill(255);
+    ellipse(0, 0, this.size, this.size);
+
+    rotate(this.angle);
+
+    fill(153, 204, 0);
+    ellipse(this.size / 4, 0, this.size / 2, this.size / 2);
+
+    pop();
+  };
+}
